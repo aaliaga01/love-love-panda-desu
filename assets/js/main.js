fe236d8581b8b0078c218dc3212259e50a56a7b2
aaliaga-casa <a.aliaga.val@gmail.com>
2017-05-19 20:32:10 -0300

    agregando estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", 
+"assets/img/a3.jpg", "assets/img/a4.jpg"];
+
+pictures.forEach(function(ruta){
+	
+	var foto = document.createElement("img");
+	foto.setAttribute("class","marco");
+
+	foto.setAttribute("src", ruta);
+	document.getElementById("cont-imgs").appendChild(foto);
+		}
+);
